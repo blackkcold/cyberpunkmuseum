c7c8670371c1bb117cb275d505b969a279a4e17e
blackkcold <blackkcold@outlook.com>
2022-12-28 16:31:00 +0000

    new

--- components/button.vue
+++ components/button.vue
@@ -1,27 +1,76 @@
 <template>
-    <button class="btn" :style="{color:color}">
-      <slot/> <!-- 插槽 -->
-    </button>
-  </template>
+  <nav class="navbar">
+    <ul>
+      <li class="exhibition-navitem-desk"><a href="index.html">Exhibition</a></li>
+      <li class="search-navitem-desk"><a href="#">Search</a></li>
+      <li class="content-navitem-desk"><a href="Content.html">Content</a></li>
+      <li class="help-navitem-desk"><a href="Help.html">Help</a></li>
+      <li class="about-navitem-desk"><a href="About.html">About</a></li>
+    </ul>
+  </nav>
+</template>
   
-  <script>
-  export default {
-    // 传入子组件的参数写到props
-    props: {
-      color: {
-        type: String, // 颜色参数类型
-        default: "#000"  // 默认黑色
-      }
+<script>
+export default {
+  // 传入子组件的参数写到props
+  props: {
+    color: {
+      type: String, // 颜色参数类型
+      default: "#000"  // 默认黑色
     }
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-    .btn {
-      width: 200px;
-      height: 200px;
-      border-radius: 10px;
-      border: none;
-      font-size: 15px;
-    }
-  </style>
+<style scoped>
+.navbar {
+  margin-right: 30px;
+
+  transition: 1s;
+}
+
+.navbar ul {
+  display: flex;
+  justify-items: right;
+
+  list-style-type: none;
+
+  padding: 0px;
+
+  transition: 1s;
+}
+
+.navbar a {
+  font-weight: 900;
+  font-size: 12px;
+  text-decoration: none;
+  color: white;
+
+  padding: 10px 20px;
+  margin: 0px 20px;
+
+  transition: 1s;
+}
+
+.navbar li {
+  transition: 1s;
+}
+
+.navbar a:hover {
+  color: black;
+
+  background-color: #f3ed19;
+}
+
+.navbar li:hover {
+  -ms-transform: rotate(-15deg);
+  /* IE 9 */
+  -moz-transform: rotate(-15deg);
+  /* Firefox */
+  -webkit-transform: rotate(-15deg);
+  /* Safari & Chrome */
+  -o-transform: rotate(-15deg);
+  /* General Usage */
+  transform: rotate(-15deg);
+}
+</style>
