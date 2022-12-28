9d43466a4e7bcd3f487bf7dcccfe1ebd78247ae1
blackkcold <blackkcold@outlook.com>
2022-12-28 01:53:17 +0000

    all pages added
    添加了大部分界面，仅剩exhibition和about需要完成，在研究引入vue做数据绑定和组件化

---
+++ components/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="btn" :style="{color:color}">
+      <slot/> <!-- 插槽 -->
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    // 传入子组件的参数写到props
+    props: {
+      color: {
+        type: String, // 颜色参数类型
+        default: "#000"  // 默认黑色
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+    .btn {
+      width: 200px;
+      height: 200px;
+      border-radius: 10px;
+      border: none;
+      font-size: 15px;
+    }
+  </style>
